015b4ccd51d146697be06f715b2a7b7d5a3bd973
Wilmer <wil.martinez0420@gmail.com>
2019-08-21 19:31:29 -0500

    Ciclo números primos y validación

--- Primo/main.test.js
+++ Primo/main.test.js
@@ -1,10 +1,5 @@
 const calcular = require('./main');
 
 test('Si el número es 3 retorna true', () => {
-  expect(calcular(3)).toBe(0);
-});
-
-
-test('La función da si es número o no', () => {
-  expect(calcular(7)).toBe(1);
+  expect(calcular(3)).toBe(1);
 });
